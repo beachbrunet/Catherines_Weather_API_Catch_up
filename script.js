dac897c1d5070afd7c1f5a13f23f223368a6dde3
Catherine Cox <ccox.034@gmail.com>
2021-08-20 09:18:29 -0600

    added API key

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const HISTORY_KEY = "#";
 const HISTORY_BTN = "#";
 const HISTORY_DATA_CITY = "#";
-const API_KEY = "#;
+const API_KEY = "78e6fa2e20b4c1189daa386837eff4b5";
 
 //Event search button to display/ event listener
 $(document).on('click', ".city", function () {
@@ -28,7 +28,7 @@ displayWeatherInfo (showCity);
 //     let cities =JSON.parse(localstorage.getItem("city-list")) || [];
 // })
 
-
+var weatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={78e6fa2e20b4c1189daa386837eff4b5}'
 
 
 
