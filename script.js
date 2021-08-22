0010ba39d491287aa7bd12aecac3c9a3daae9b6c
Catherine Cox <ccox.034@gmail.com>
2021-08-22 17:14:19 -0600

    debugging undefined

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "
     
 //  API Call is: let weatherURL = "api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apiKey;
 // Api Key is: 78e6fa2e20b4c1189daa386837eff4b5
-console.log("sec"+searchedCity);
+console.log("cities"+searchedCity);
 localStorage.setItem("storedCities", JSON.stringify(searchedCity));
 
-function displayWeather(data){
+// stores the cities
+function showWeather(data){
     $("#search-city").empty();
     //for loop to add cities
     for (let i = 0; i < searchCities.length; i++) {
@@ -17,10 +18,9 @@ function displayWeather(data){
       $("#search-city").append(listEl);
     };
 
-
+// calls on the API depending on results
 function apiCallsWeather(city){
-    let getCity = this.getCity;
-}
+    
 
 $.ajax({
     url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY,
@@ -34,7 +34,7 @@ $.ajax({
           })
           .then(function (data) {   
             console.log(data); 
-            displayWeather(data);
+            showWeather(data);
           })
           .catch((error) => {
             console.error("FETCH ERROR:", error);
@@ -43,40 +43,38 @@ $.ajax({
           $.ajax({
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + lat + "&lon=" + lon+ "&units=imperial&exclude=minutely,hourly&appid=" + API_KEY,
             method: "GET"
-          }).then(function(data){
-            displayWeather(data)
-          });
+          }).then(showWeatherData);
         }
+    }
 
 
 // Create container for weather to populate in
-$("display-data").append($(",H2 id='forcast-header'>").text("Your 5 day Forcast"));
-let containerEL =$("<div class='row cards' id='5-day>");
+$("display-data").append($("<H2 id='forcast-header'>").text("Your 5 day Forcast"));
+let containerEL =$("<div class='rows' id='5-day>");
 
 
 // Event listener
-$("searchBtn").on("click", function(event){
+$("#searchBtn").on("click", function(event){
     event.preventDefault();
-    city =$("inputCity")
-    });
+    city = $("#inputCity");
+    apiCallsWeather(city);
+});
 
 
 //     // requirements THEN I am presented with the city name, the date, an icon representation of weather conditions,
 //     // the temperature, the humidity, the wind speed, and the UV index
-    let cityWeatherData = data;
+   function showWeatherData(cityWeatherData) {
     let currentDate = moment().format('MM/DD/YYYY');
     let dataEL = $("<div class='jumbotron' id='jumbotron'>");
     dataEL.append($("<h1 class='header>").text(cityWatherData + " "+currentDate));
-    dataEL.append($("<p>").text("Tempurature" +cityWatherData.current.temp));
-    dataEL.append($("<p>").text("Humidity" +cityWatherData.current.humidity));
-    dataEL.append($("<p>").text("Wind Speed" +cityWatherData.current.wind_speed));
-    dataEL.append($("<p>").text("UV index" +cityWatherData.current.uvi));
-
-
-
+    dataEL.append($("<p>").text("Tempurature" +cityWeatherData.current.temp));
+    dataEL.append($("<p>").text("Humidity" +cityWeatherData.current.humidity));
+    dataEL.append($("<p>").text("Wind Speed" +cityWeatherData.current.wind_speed));
+    dataEL.append($("<p>").text("UV index" +cityWeatherData.current.uvi));
+   }
 
 // // Reset button
-resetBtn.addEventListener("click", function() {
-    window.localStorage.clear();
-    window.location.reload();
-})
+// resetBtn.addEventListener("click", function() {
+//     window.localStorage.clear();
+//     window.location.reload();
+// })
