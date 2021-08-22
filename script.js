e4571e877784fd19b40b25c00e2d9ffde09c5798
Catherine Cox <ccox.034@gmail.com>
2021-08-22 09:46:19 -0600

    Added reset button, weather function, containter and event listener

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
-//Create a function to create buttons on previously searched cities (local storage)
-
 //Varriables
-const HISTORY_KEY = "#";
-const HISTORY_BTN = "#";
-const HISTORY_DATA_CITY = "#";
-const API_KEY = "78e6fa2e20b4c1189daa386837eff4b5";
+let city = "";
+let searchedCity = [];
+let currentDate = moment().format('MM/DD/YYYY');
+const apiKey = "78e6fa2e20b4c1189daa386837eff4b5";
+    
+//  API Call is: 
+// Api Key is: 78e6fa2e20b4c1189daa386837eff4b5
 
-//Event search button to display/ event listener
-$(document).on('click', ".city", function () {
 
+// function to get weather
+
+function getWeather() {
+        let weatherURL = "api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apiKey;
+        fetch(weatherURL)
+        .then(function(response){
+        return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+    })
+    
+};
+
+
+// Create container for weather to populate in
+$("display-data").append($(",H2 id='forcast-header'>").text("Your 5 day Forcast"));
+let containerEL =$("<div class='row cards' id='5-day>");
+
+
+
+
+// Event listener
+$("searchBtn").on("click, function(event){
+    event.preventDefault();
 }
 
 
-//Display weather
-displayWeatherInfo (showCity);
 
-
-
-//Function to return search weather data
 
 // Reset button
-
-
-
-// $(document).ready (function () {
-// // citties parsed from local storage. Default to empty if no history searched
-//     let cities =JSON.parse(localstorage.getItem("city-list")) || [];
-// })
-
-var weatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={78e6fa2e20b4c1189daa386837eff4b5}'
-
-
-
-
-
-
-
-
-
-
+resetBtn.addEventListener("click", function() {
+    window.localStorage.clear();
+    window.location.reload();
+})
