5e6069065224a720141610822a606dc8de3c528a
Catherine Cox <87046645+beachbrunet@users.noreply.github.com>
2021-09-28 14:22:02 -0600

    added README

---
+++ README.md
@@ -0,0 +1,57 @@
+# Catherines_Weather_API_Catch_up
+
+## Requirements
+
+<!-- ----------------------- -->
+
+```
+If you choose to accept your mission....
+
+Use the OpenWeather One Call API to retrieve weather data for cities. Read through the documentation for setup and usage instructions. You will use `localStorage` to store any persistent data.
+
+
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+
+## Screenshot
+
+<!-- ----------------------- -->
+
+![Screen Shot]()
+
+## Recources and Technologies
+
+<!-- ----------------------- -->
+
+<!-- https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys
+https://openweathermap.org/api/one-call-api
+https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} -->
+
+[OpenWeather One Call API](https://openweathermap.org/api/one-call-api)
+
+[Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys)
+
+[BootStrap](https://getbootstrap.com/)
+
+[jQuery](https://jquery.com)
+
+## Contributions and Contact Information
+
+<!-- ----------------------- -->
+
+Catherine Cox
+ccox.034@gmail.com
--- script.js
+++ script.js
@@ -2,78 +2,96 @@
 let city = "";
 let searchedCity = [];
 const API_KEY = "78e6fa2e20b4c1189daa386837eff4b5";
-const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY;
-    
+const queryURL =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  API_KEY;
+
 //  API Call is: let weatherURL = "api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apiKey;
 // Api Key is: 78e6fa2e20b4c1189daa386837eff4b5
-console.log("cities"+searchedCity);
+console.log("cities" + searchedCity);
 localStorage.setItem("storedCities", JSON.stringify(searchedCity));
 
 // stores the cities
-function showWeather(data){
-    $("#search-city").empty();
-    //for loop to add cities
-    for (let i = 0; i < searchCities.length; i++) {
-      var listEl = $("<li class='list-group-item'>").text(searchCities[i]);
-      $("#search-city").append(listEl);
-    };
+function showWeather(data) {
+  $("#search-city").empty();
+  //for loop to add cities
+  for (let i = 0; i < searchCities.length; i++) {
+    var listEl = $("<li class='list-group-item'>").text(searchCities[i]);
+    $("#search-city").append(listEl);
+  }
 }
 // calls on the API depending on results
-function apiCallsWeather(city){
-    
-
-$.ajax({
-    url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY,
-    method: "GET"
-}).then(function(data){
-    if (data.ok) {
-              return data.json();
-            } else {
-              throw new Error("NETWORK RESPONSE NOT OK");
-            }
-          })
-          .then(function (data) {   
-            console.log(data); 
-            showWeather(data);
-          })
-          .catch((error) => {
-            console.error("FETCH ERROR:", error);
-          });
-// lat and lon needed 
-          $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/weather?q=" + lat + "&lon=" + lon+ "&units=imperial&exclude=minutely,hourly&appid=" + API_KEY,
-            method: "GET"
-          }).then(showWeatherData);
-        }
-
+function apiCallsWeather(city) {
+  $.ajax({
+    url:
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      API_KEY,
+    method: "GET",
+  })
+    .then(function (data) {
+      if (data.ok) {
+        return data.json();
+      } else {
+        throw new Error("NETWORK RESPONSE NOT OK");
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+      showWeather(data);
+    })
+    .catch((error) => {
+      console.error("FETCH ERROR:", error);
+    });
+  // lat and lon needed
+  $.ajax({
+    url:
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&units=imperial&exclude=minutely,hourly&appid=" +
+      API_KEY,
+    method: "GET",
+  }).then(showWeatherData);
+}
 
 // Create container for weather to populate in
-$("display-data").append($("<H2 id='forcast-header'>").text("Your 5 day Forcast"));
-let containerEL =$("<div class='rows' id='5-day>");
-
+$("display-data").append(
+  $("<H2 id='forcast-header'>").text("Your 5 day Forcast")
+);
+let containerEL = $("<div class='rows' id='5-day>");
 
 // Event listener
-$("#searchBtn").on("click", function(event){
-    event.preventDefault();
-    city = $("#inputCity").val();
-    apiCallsWeather(city);
+$("#searchBtn").on("click", function (event) {
+  event.preventDefault();
+  city = $("#inputCity").val();
+  apiCallsWeather(city);
 });
-
 
 //     // requirements THEN I am presented with the city name, the date, an icon representation of weather conditions,
 //     // the temperature, the humidity, the wind speed, and the UV index
-   function showWeatherData(cityWeatherData) {
-    let currentDate = moment().format('MM/DD/YYYY');
-    let dataEL = $("<div class='jumbotron' id='jumbotron'>");
-    dataEL.append($("<h1 class='header>").text(cityWatherData + " "+currentDate));
-    dataEL.append($("<p>").text("Tempurature" +cityWeatherData.current.temp));
-    dataEL.append($("<p>").text("Humidity" +cityWeatherData.current.humidity));
-    dataEL.append($("<p>").text("Wind Speed" +cityWeatherData.current.wind_speed));
-    dataEL.append($("<p>").text("UV index" +cityWeatherData.current.uvi));
-   }
+function showWeatherData(cityWeatherData) {
+  let currentDate = moment().format("MM/DD/YYYY");
+  let dataEL = $("<div class='jumbotron' id='jumbotron'>");
+  dataEL.append(
+    $("<h1 class='header>").text(cityWatherData + " " + currentDate)
+  );
+  dataEL.append($("<p>").text("Tempurature" + cityWeatherData.current.temp));
+  dataEL.append($("<p>").text("Humidity" + cityWeatherData.current.humidity));
+  dataEL.append(
+    $("<p>").text("Wind Speed" + cityWeatherData.current.wind_speed)
+  );
+  dataEL.append($("<p>").text("UV index" + cityWeatherData.current.uvi));
+}
 
 // // Reset button
 // resetBtn.addEventListener("click", function() {
 //     window.localStorage.clear();
 //     window.location.reload();
 // })
+TO DO
+// LAT ISNT DEFINED
